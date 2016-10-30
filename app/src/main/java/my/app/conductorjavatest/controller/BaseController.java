c7790e886e1a08ca0c2f661a762ed6b22c870882
Den Drobiazko <den.d@techery.io>
2016-10-30 19:22:10 +0200

    Add state saving&restoring to base controller class, fix code style

--- app/src/main/java/my/app/conductorjavatest/controller/BaseController.java
+++ app/src/main/java/my/app/conductorjavatest/controller/BaseController.java
@@ -1,5 +1,6 @@
 package my.app.conductorjavatest.controller;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -12,51 +13,64 @@ import com.hannesdorfmann.mosby.mvp.conductor.MvpController;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import icepick.Icepick;
 import my.app.conductorjavatest.Layout;
 
 public abstract class BaseController<V extends MvpView, P extends MvpPresenter<V>> extends MvpController<V, P> {
 
-    private Unbinder unbinder;
+   private Unbinder unbinder;
 
-    @NonNull
-    @Override
-    protected View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container) {
-        Layout layout = getLayoutFromAnnotation(this.getClass());
-        if (layout == null) {
-            throw new IllegalArgumentException("ConfigurableFragment should have Layout annotation");
-        }
+   @NonNull
+   @Override
+   protected View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container) {
+      Layout layout = getLayoutFromAnnotation(this.getClass());
+      if (layout == null) {
+         throw new IllegalArgumentException("ConfigurableFragment should have Layout annotation");
+      }
 
-        View view = inflater.inflate(layout.value(), container, false);
-        unbinder = ButterKnife.bind(this, view);
-        onViewBound(view);
-        return view;
-    }
+      View view = inflater.inflate(layout.value(), container, false);
+      unbinder = ButterKnife.bind(this, view);
+      onViewBound(view);
+      return view;
+   }
 
-    protected void onViewBound(@NonNull View view) {
-    }
+   protected void onViewBound(@NonNull View view) {
+   }
 
-    @Override
-    protected void onDestroyView(View view) {
-        super.onDestroyView(view);
-        unbinder.unbind();
-        unbinder = null;
-    }
+   @Override
+   protected void onDestroyView(View view) {
+      super.onDestroyView(view);
+      unbinder.unbind();
+      unbinder = null;
+   }
 
-    /**
-     * Recursively scans class hierarchy searching for {@link Layout} annotation defined.
-     *
-     * @param clazz class to search for annotation
-     * @return defined layout if any or <b>null</b>
-     */
-    @Nullable
-    private Layout getLayoutFromAnnotation(Class clazz) {
-        if (clazz == null || clazz.equals(Object.class)) return null;
+   @Override
+   protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+      super.onRestoreInstanceState(savedInstanceState);
+      Icepick.restoreInstanceState(this, savedInstanceState);
+   }
 
-        Layout layout = (Layout) clazz.getAnnotation(Layout.class);
-        if (layout != null) {
-            return layout;
-        } else {
-            return getLayoutFromAnnotation(clazz.getSuperclass());
-        }
-    }
+   @Override
+   protected void onSaveInstanceState(@NonNull Bundle outState) {
+      super.onSaveInstanceState(outState);
+      Icepick.saveInstanceState(this, outState);
+   }
+
+   /**
+    * Recursively scans class hierarchy searching for {@link Layout} annotation defined.
+    *
+    * @param clazz class to search for annotation
+    * @return defined layout if any or <b>null</b>
+    */
+   @Nullable
+   private Layout getLayoutFromAnnotation(Class clazz) {
+      if (clazz == null || clazz.equals(Object.class)) return null;
+
+      Layout layout = (Layout) clazz.getAnnotation(Layout.class);
+      if (layout != null) {
+         return layout;
+      } else {
+         return getLayoutFromAnnotation(clazz.getSuperclass());
+      }
+   }
 }
